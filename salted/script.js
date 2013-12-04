ca7b60f22f462a6c0f40783a2a292bb69bfcb05e
Karan <kp08sk@gmail.com>
2013-12-04 13:03:05 -0600

    changes

---
+++ salted/script.js
@@ -0,0 +1,19 @@
+
+
+function toggle_messege(type) {
+ document.getElementById("f1").style.display = type;
+    document.getElementById("f101").style.display = type;
+
+}
+
+function toggle_messege(type) {
+ document.getElementById("f2").style.display = type;
+    document.getElementById("f102").style.display = type;
+
+}
+
+
+--*//
+
+
+
