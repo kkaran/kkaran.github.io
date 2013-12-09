5c5db9e78a296c3b335ef57c7714e472339d9d4a
Karan <kp08sk@gmail.com>
2013-12-08 23:12:34 -0600

    tomIsAwesome

--- Signup-MXL/script.js
+++ Signup-MXL/script.js
@@ -1,9 +1,17 @@
-	$(function(){
-			    $("#site").cycle({
+	
+
+$('next-button').on('click', function(){
+
+			    $(".tomIsAwesome")({
 			        fx : "scrollVert",
-			        next : ".moveUp",
+			        next : ".moveUp a",
 			        prev : ".moveDown a",
 			        startingSlide : 1,
 			        timeout : 0
 			    });
 			  });
+
+
+$('current-div').hide();
+$('next-div').show(0).delay(5000);
+});
