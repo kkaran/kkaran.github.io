20dd746876167b27fe022ee06a331f220bbc2aa5
Karan <kp08sk@gmail.com>
2013-12-08 18:40:33 -0600

    One page

---
+++ Signup-MXL/script.js
@@ -0,0 +1,9 @@
+	$(function(){
+			    $("#site").cycle({
+			        fx : "scrollVert",
+			        next : ".moveUp a",
+			        prev : ".moveDown a",
+			        startingSlide : 1,
+			        timeout : 0
+			    });
+			  });
