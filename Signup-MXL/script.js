b973cbbf492c036d347c2985a9c5cd4ce55d7793
Karan <kp08sk@gmail.com>
2013-12-08 23:58:38 -0600

    hide

--- Signup-MXL/script.js
+++ Signup-MXL/script.js
@@ -1,6 +1,14 @@
+/*--
 function nextPage(curDiv, nextDiv){
 $(curDiv).css('display', 'none');
 $(nextDiv).css('display', 'block');
+}
+*/
+
+
+function nextPage(curDiv, nextDiv){
+$(curDiv).hide();
+$(nextDiv).show(0).delay(5000);
 }
 
 
@@ -8,4 +16,3 @@ $('nextButtonOne').on('click', nextPage('.tomIsAwesomeOne', '.tomIsAwesomeTwo'))
 $('nextButtonTwo').on('click', nextPage('.tomIsAwesomeTwo', '.tomIsAwesomeThree'));
 $('nextButtonThree').on('click', nextPage('.tomIsAwesomeThree', '.tomIsAwesomeFour'));
 $('nextButtonFour').on('click', nextPage('.tomIsAwesomeFour', '.tomIsAwesomeFive'));
-
