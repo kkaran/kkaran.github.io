f59eaa98dcbfc4167370db8b983330f9cbf4e44d
Karan <kp08sk@gmail.com>
2014-01-21 20:53:26 -0600

    Coming along

---
+++ Portfolio_3.0/js/autoScrollTo.js
@@ -0,0 +1,65 @@
+<!-- autoScrollTo.js ... logic for a lightweight reusable external framework -->
+
+
+
+// autoScrollTo.js Written By Adam Khoury - Nov 2012
+
+var scrollY = 0;
+var distance = 40;
+var speed = 24;
+function autoScrollTo(el) {
+	var currentY = window.pageYOffset;
+	var targetY = document.getElementById(el).offsetTop;
+	var bodyHeight = document.body.offsetHeight;
+	var yPos = currentY + window.innerHeight;
+	var animator = setTimeout('autoScrollTo(\''+el+'\')',24);
+	if(yPos > bodyHeight){
+		clearTimeout(animator);
+	} else {
+		if(currentY < targetY-distance){
+		    scrollY = currentY+distance;
+		    window.scroll(0, scrollY);
+	    } else {
+		    clearTimeout(animator);
+	    }
+	}
+}
+function resetScroller(el){
+	var currentY = window.pageYOffset;
+    var targetY = document.getElementById(el).offsetTop;
+	var animator = setTimeout('resetScroller(\''+el+'\')',speed);
+	if(currentY > targetY){
+		scrollY = currentY-distance;
+		window.scroll(0, scrollY);
+	} else {
+		clearTimeout(animator);
+	}
+}
+
+function scrollProcess(el){
+	var targetY = document.getElementById(el).offsetTop;
+	var currentY = window.pageYOffset;
+	distance = (targetY -currentY) / 20;
+	if (distance >0 ) {autoScrollTo(el);}
+	else {autoScrollUp(el);}
+
+}
+function autoScrollUp(el) {
+	var currentY = window.pageYOffset;
+	var targetY = document.getElementById(el).offsetTop;
+	var bodyHeight = document.body.offsetHeight;
+	var yPos = currentY + window.innerHeight;
+	var animator = setTimeout('autoScrollUp(\''+el+'\')',24);
+	if(yPos <=0){
+		clearTimeout(animator);
+	} else {
+		if(currentY > targetY-distance){
+		    scrollY = currentY+distance;
+		    window.scroll(0, scrollY);
+	    } else {
+		    clearTimeout(animator);
+	    }
+	}
+}
+
+
