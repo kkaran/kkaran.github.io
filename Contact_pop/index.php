110f31f4b7eb909ba36e45359b51d496195335f5
Karan <kp08sk@gmail.com>
2014-01-31 15:18:25 -0600

    test

--- Contact_pop/index.php
+++ Contact_pop/index.php
@@ -12,10 +12,7 @@
     if ($_POST['submit'] {				 
         if (mail ($to, $subject, $body, $from)) { 
 	    echo '<p>Your message has been sent!</p>';
-	} else { 
-	    echo '<p>Something went wrong, go back and try again!</p>'; 
 	} 
-    
 
 ?>
 
