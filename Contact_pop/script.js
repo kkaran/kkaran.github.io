c08d6ecaf9d0aebb295734ce4629f295dbfbf927
Karan <kp08sk@gmail.com>
2014-01-31 14:50:29 -0600

    testing email

--- Contact_pop/script.js
+++ Contact_pop/script.js
@@ -1,4 +1,4 @@
-     <!-- CONTACT TOGGLE NAVIGATION -->
+     <!-- CONTACT TOGGLE -->
       $(document).ready(function() {
            var navwidth = ($("#contact_form").width());
                 $(".contact_show").click(function() {
@@ -8,14 +8,8 @@
                 $(".contact_show");
       });
         
-        $("#contact_form").click(function() {
-            var navwidth = ($("#contact_form").width());
-                $(".site").delay(500).animate({"left": 0}, 750),
-                $("#contact_form").delay(200).animate({marginRight: - navwidth}, 400),
-                $(".contact_show").delay(100).fadeIn(300);
-        });
-        
-        $(".close_navigation").click(function() {
+    
+        $(".close").click(function() {
             var navwidth = ($("#contact_form").width());
                 $("#contact_form").animate({marginRight: - navwidth}, 50).fadeOut(1000),
                 $(".close").fadeOut(400),
