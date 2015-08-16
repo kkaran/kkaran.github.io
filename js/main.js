8acf35eca0d0c4a48ae639422969c55af5ae8ef6
Karan Patel <kp08sk@gmail.com>
2015-08-16 13:36:50 -0500

    Added js files
    Move JS into separate file / cleaned up code

---
+++ js/main.js
@@ -0,0 +1,56 @@
+
+$(document).ready(function() {
+// Nav bar icon animation --- //
+
+	$( ".nav_icon" ).click(function() {
+	    //alert($( this ).css( "transform" ));
+	    if (  $( this ).css( "transform" ) == 'none' ){
+	        $(this).css("transform","rotate(90deg)");
+	    } else {
+	        $(this).css("transform","" );
+	    }
+	});
+});
+
+
+//Progress Bar Js --- //  
+	$(document).ready(function() {
+		$('progress').attr ({
+			max: $(document).height() - $(window).height()
+		});
+	});
+
+	$(document).scroll (function() {
+		$('progress').attr({
+	        value: $(window).scrollTop()
+	      });
+	});
+
+	$(document).resize (function() {
+		$('progress').attr({
+	        max: $(document).height() - $(window).height(),
+	        value: $(window).scrollTop()
+	      });
+	});
+
+// Time of Day Js --- 
+	$(document).ready(function() {
+		sayHello();
+		setInterval('sayHello()', 10000);
+		});
+
+	function sayHello() {
+		var now = new Date();
+		var month = now.getMonth();
+		var date = now.getDate();
+		var hours = now.getHours();
+		var msg;
+		if (hours < 12) {
+		   	msg = "good morning.";
+		} else if (hours < 18) {
+		   	msg = "good afternoon.";
+		} else {
+		  	msg = "good evening.";
+		}
+		$('#header_hello h2').text(msg);
+		}
